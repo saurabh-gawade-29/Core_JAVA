28bcac2a7c674acfc4309763e882dfbcfdd8d582
Saurabh Gawade <saurabh.g@finacus.co.in>
2022-07-14 13:41:19 +0530

    String Method Add

---
+++ Lecture3/StringMethods.java
@@ -0,0 +1,22 @@
+package Lecture3;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        System.out.println("Please Refer Link in Comments");
+//      https://www.w3schools.com/java/java_ref_string.asp
+
+/*
+* charAt()	Returns the character at the specified index (position)
+* compareTo()	Compares two strings lexicographically
+* concat()	Appends a string to the end of another string
+* contains()	Checks whether a string contains a sequence of characters
+* endsWith()	Checks whether a string ends with the specified character(s)
+* isEmpty()	Checks whether a string is empty or not
+* replace()	Searches a string for a specified value, and returns a new string where the specified values are replaced
+* replaceAll()	Replaces each substring of this string that matches the given regular expression with the given replacement
+* split()	Splits a string into an array of substrings
+* toString()	Returns the value of a String object
+* trim()	Removes whitespace from both ends of a string
+* */
+    }
+}
