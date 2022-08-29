72fcb319766f4be7d96575c8eed3f58235d9d260
saurabh gawade <saurabhgawade2920@gmail.com>
2022-08-29 18:42:44 +0530

    update lecture 22

---
+++ Lecture22/Depersist.java
@@ -0,0 +1,16 @@
+package Lecture22;
+
+import java.io.*;
+class Depersist{
+    public static void main(String args[]){
+        try{
+            //Creating stream to read the object
+            ObjectInputStream in=new ObjectInputStream(new FileInputStream("f.txt"));
+            Student s=(Student)in.readObject();
+            //printing the data of the serialized object
+            System.out.println(s.id+" "+s.name);
+            //closing the stream
+            in.close();
+        }catch(Exception e){System.out.println(e);}
+    }
+}
---
+++ Lecture22/Student.java
@@ -0,0 +1,10 @@
+package Lecture22;
+import java.io.Serializable;
+public class Student  implements Serializable {
+    int id;
+    String name;
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
